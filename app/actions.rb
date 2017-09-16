966b07e1af754d66b206423e13b6610e77519975
Frederick Scott <frederick.william.scott@gmail.com>
2017-09-16 02:31:53 +0000

    hash incorporate method into post

--- app/actions.rb
+++ app/actions.rb
@@ -11,14 +11,13 @@ get '/' do
         username: "frederick_s",
         avatar_url: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAssAAAAJDE1ODI2N2M1LTYwOTgtNDY2YS04YTdmLTEyMmY1Yzk1YzlmYQ.jpg",
         photo_url: "http://static1.squarespace.com/static/56e0f44160b5e96aec2019f3/t/5704feaa7da24f6f4e0265de/1459945131814/espresso-09.jpg?format=1000w",
-        time_ago_in_minutes: 120,
+        humanized_time_ago: humanized_time_ago(15),
         like_count: 0,
         comment_count: 1,
         comments: [{
             username: "frederick_s",
-            text: "frederick_s: Espresso is coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground coffee beans."
+            text: "test"
         }]
     }
-    humanized_time_ago(post[:time_ago_in_minutes])
 end
 
