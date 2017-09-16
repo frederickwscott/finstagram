58f072dd592b33e2310c81ab55ad4f4e7ad4a588
Frederick Scott <frederick.william.scott@gmail.com>
2017-09-16 02:38:59 +0000

    working: before incorporating method into call humanized_time_ago into the post hash

--- app/actions.rb
+++ app/actions.rb
@@ -11,13 +11,14 @@ get '/' do
         username: "frederick_s",
         avatar_url: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAssAAAAJDE1ODI2N2M1LTYwOTgtNDY2YS04YTdmLTEyMmY1Yzk1YzlmYQ.jpg",
         photo_url: "http://static1.squarespace.com/static/56e0f44160b5e96aec2019f3/t/5704feaa7da24f6f4e0265de/1459945131814/espresso-09.jpg?format=1000w",
-        humanized_time_ago: humanized_time_ago(15),
+        time_ago_in_minutes: 120,
         like_count: 0,
         comment_count: 1,
         comments: [{
             username: "frederick_s",
-            text: "test"
+            text: "Espresso is coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground coffee beans."
         }]
     }
+    humanized_time_ago(post[:time_ago_in_minutes])
 end
 
