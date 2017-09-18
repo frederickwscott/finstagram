42f728cb24235c4445c792167d3c55b850021710
Frederick Scott <frederick.william.scott@gmail.com>
2017-09-18 03:43:48 +0000

    Finstagram: Ruby Basics - completed

--- app/actions.rb
+++ app/actions.rb
@@ -7,11 +7,11 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-    post = {
+    post_frederick = {
         username: "frederick_s",
         avatar_url: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAssAAAAJDE1ODI2N2M1LTYwOTgtNDY2YS04YTdmLTEyMmY1Yzk1YzlmYQ.jpg",
         photo_url: "http://static1.squarespace.com/static/56e0f44160b5e96aec2019f3/t/5704feaa7da24f6f4e0265de/1459945131814/espresso-09.jpg?format=1000w",
-        time_ago_in_minutes: 120,
+        humanized_time_ago: humanized_time_ago(15),
         like_count: 0,
         comment_count: 1,
         comments: [{
@@ -19,6 +19,33 @@ get '/' do
             text: "Espresso is coffee brewed by forcing a small amount of nearly boiling water under pressure through finely ground coffee beans."
         }]
     }
-    humanized_time_ago(post[:time_ago_in_minutes])
+    
+    post_whale = {
+        username: "kirk_whalum",
+        avatar_url: "https://naserca.com/images/kirk_whalum.jpg",
+        photo_url: "http://naserca.com/images/whale.jpg",
+        humanized_time_ago: humanized_time_ago(65),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "kirk_whalum",
+            text: "#weekendvibes"
+        }]
+    }
+    
+    post_marlin = {
+        username: "marlin_peppa",
+        avatar_url: "https://naserca.com/images/marlin_peppa.jpg",
+        photo_url: "http://naserca.com/images/marlin.jpg",
+        humanized_time_ago: humanized_time_ago(190),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "marlin_peppa",
+            text: "lunchtime! ;)"
+        }]
+    }
+    
+    [post_frederick, post_whale, post_marlin].to_s
 end
 
